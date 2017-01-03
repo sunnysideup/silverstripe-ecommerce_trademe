6b500cfe8148c592515a6d79641ad84ffd87fdae
sunnysideup <sunnysideupdevs@gmail.com>
2017-01-03 14:12:31 +1300

    PATCH: module clean-up

---
+++ tests/EcommerceTrademeTest.php
@@ -0,0 +1,13 @@
+<?php
+class EcommerceTrademeTest extends SapphireTest {
+
+	protected $usesDatabase = false;
+
+	protected $requiredExtensions = array();
+
+	public function testMyMethod() {
+		$this->assertEquals(1, 1);
+	}
+
+}
+
